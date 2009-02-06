0a2bbcac561473a7cc9651b5f28927145c0f0d2e
Nicola Brisotto <nicolabrisotto@gmail.com>
2009-02-06 12:08:19 +0100

    initial import

---
+++ lib/replace.rb
@@ -0,0 +1,16 @@
+if ARGV.size != 1
+        puts "USAGE: replace -i <input> -r <regexp> "
+        exit
+end
+
+if !File::exist?(ARGV[0]) || File.new(ARGV[0]).stat.directory?
+        puts "ERROR the argument must be a directory"
+        exit
+end
+
+File.open(ARGV[0]) do |i|
+  while( a = i.gets )
+    puts a.sub(/.*/, '"\0\n"');
+  end
+end
+
